cc33d91cc6777dcb34564f4ad976bc8a2d09c512
Marcio da Silva Eduardo <marcioeduardo.ti@gmail.com>
2021-12-01 13:25:30 -0300

    Concluida a estrutura do app

--- IdadeCanina/ViewController.swift
+++ IdadeCanina/ViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblResultado: UILabel!
+    
+    
+    @IBOutlet weak var txtCampoIdadeCachorro: UITextField!
+    
+    
+    @IBAction func btnDescobreIdade(_ sender: Any) {
+        let idade = Int(txtCampoIdadeCachorro.text!)! * 7
+        lblResultado.text = "A idade do cachorro é: " + String(idade)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
